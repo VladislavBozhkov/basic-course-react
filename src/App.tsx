9f5ed697f670da7f083498e6f093335209dcc42f
Bozhkov.v <Bozhkov.v@dobrozaim.ru>
2025-01-28 17:40:37 +0300

    вывод диномических данных, пропсы

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,58 @@
-import Header from "./components/Header"
+import React from 'react';
+import Header from './components/Header';
 
 
-function App() {
-  
 
-  return (
-    <Header />
+
+const products: Product[] = [
+    {
+        title: 'молоко',
+        price: '100р'
+    },
+    {
+        title: 'хлеб',
+        price: '50р'
+    },
+    {
+        title: 'яйца',
+        price: '120р'
+    },
+    {
+        title: 'сыр',
+        price: '200р'
+    }
+];
+
+interface Product {
+  title : string
+  price : string
+} 
+
+interface WayToTeachProps {
+  products : Product[]
+}
+
+const WayToTeach: React.FC<WayToTeachProps> = ({products}) => {
+  return(
+    
+    <ul>
+      {products.map((product) => (
+        <li>
+          <span> {product.title} - {product.price}</span>
+        </li>
+      ))}
+    </ul>
   )
 }
 
-export default App
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <Header />
+            <WayToTeach products={products} />
+        </div>
+    );
+};
+
+export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import './Header.css'
 
 export default function Header() {
+
+    const now = new Date()
+
   return (
-    <div>Header</div>
+    <header>
+        <h3>Logo</h3>
+
+        <span>время сейчас : {now.toLocaleDateString()} </span>
+
+    </header>
+    
   )
 }
